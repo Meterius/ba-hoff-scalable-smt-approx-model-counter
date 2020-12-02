ddd708a0e9a78d9cfedc909ef1feceab082afc41
Meterius <Meterius@users.noreply.github.com>
2020-12-02 18:34:36 +0100

    Modified verifier

--- primes/run_verify_power_two_primes.py
+++ primes/run_verify_power_two_primes.py
@@ -1,3 +1,4 @@
+from z3 import *
 from primes.generate_power_two_primes import is_prime
 import os
 
@@ -7,13 +8,20 @@ if __name__ == "__main__":
     file_in = os.path.join(code_dir, "output", "suggested_power_two_primes.txt")
     file_out = os.path.join(code_dir, "output", "verified_power_two_primes.txt")
 
+    solver = SolverFor("QF_NIA")
+
     with open(file_in, "r") as fi, open(file_out, "w") as fo:
         for line in fi.readlines():
             xs, ps = line.split(" ")
             x = int(xs, 10)
             p = int(ps, 10)
 
-            pv = is_prime(p)
+            response = solver.check(Int("x") > 1, Int("x") < p, (p % Int("x")) == 0)
+
+            if response == unknown:
+                raise ValueError("Solver responded with unknown")
+
+            pv = response == unsat
 
             if pv:
                 fo.write(f"{x} {p}\n")
