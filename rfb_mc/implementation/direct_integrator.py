409066512d7b1f3b15f3cefc75cabce059e2f25a
Meterius <Meterius@users.noreply.github.com>
2021-01-16 19:31:21 +0100

    Added threading to direct integrator that increases parallel sync usage

--- rfb_mc/implementation/direct_integrator.py
+++ rfb_mc/implementation/direct_integrator.py
@@ -1,10 +1,15 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from datetime import datetime
 from time import perf_counter
-from typing import Generic, Iterable, Type, Any
+from collections import Counter
+from typing import Generic, Iterable, Type, Any, Tuple, List, Optional
 from rfb_mc.runner import FormulaParams, RunnerBase
 from rfb_mc.integrator import IntegratorBase, IntermediateResult, Result
 from rfb_mc.scheduler import SchedulerBase
+from threading import Thread, Lock
+from queue import SimpleQueue, Empty
+
+from rfb_mc.types import RfBmcTask, RfBmcResult
 
 
 class DirectIntegratorBase(
@@ -48,6 +53,39 @@ class DirectIntegratorBase(
 
         s1 = perf_counter()
 
+        syncing_task_results_lock = Lock()
+        syncing_task_results: Counter[RfBmcTask] = Counter()
+        syncable_task_results: SimpleQueue[Optional[Tuple[RfBmcTask, RfBmcResult]]] = SimpleQueue()
+
+        def run_result_sync_thread():
+            nonlocal syncing_task_results
+
+            while True:
+                task_results: List[Optional[Tuple[RfBmcTask, RfBmcResult]]] = [syncable_task_results.get()]
+
+                while True:
+                    try:
+                        task_results.append(syncable_task_results.get_nowait())
+                    except Empty:
+                        break
+
+                filtered_task_results = [task_result for task_result in task_results if task_result is not None]
+
+                if len(filtered_task_results) > 0:
+                    scheduler.store.add_rf_bmc_results(filtered_task_results)
+
+                with syncing_task_results_lock:
+                    syncing_task_results -= Counter([
+                        task for task, _ in filtered_task_results
+                    ])
+
+                # terminate if None is found in Queue
+                if None in task_results:
+                    break
+
+        result_sync_thread = Thread(target=run_result_sync_thread, daemon=True)
+        result_sync_thread.start()
+
         try:
             # execute tasks until the algorithm stops the iteration thus indicating the final result
             while True:
@@ -59,20 +97,27 @@ class DirectIntegratorBase(
                     prev_intermediate_result = algorithm_yield.intermediate_result
                     yield prev_intermediate_result
 
-                if sum(algorithm_yield.required_tasks.values()) > 0:
-                    # get a runnable task and run it
-                    task = [task for task, count in algorithm_yield.required_tasks.items() if count > 0][0]
+                with syncing_task_results_lock:
+                    required_tasks = algorithm_yield.required_tasks - syncing_task_results
+
+                if sum(required_tasks.values()) > 0:
+                    task = next(required_tasks.elements())
 
                     s = perf_counter()
                     result = runner.rf_bmc(task)
                     self._print_debug(f"Ran {task} returning {result} which took {perf_counter() - s:.3f} seconds")
 
-                    # add all result to the store
-                    # TODO: handle adding the result blocking the integrator from running more tasks
-                    scheduler.store.add_rf_bmc_results([(task, result)])
+                    with syncing_task_results_lock:
+                        syncing_task_results += Counter([(task, result)])
+
+                    syncable_task_results.put((task, result))
         except StopIteration as err:
             d1 = perf_counter() - s1
             self._print_debug(f"Running schedulers tasks until result was available took {d1:.2f} seconds")
             self._print_debug(f"Result: {err.value}")
+
+            # terminates the syncing thread
+            syncable_task_results.put(None)
+            result_sync_thread.join()
 
             return err.value
